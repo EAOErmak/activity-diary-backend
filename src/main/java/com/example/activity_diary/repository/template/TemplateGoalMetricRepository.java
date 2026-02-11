17a3af6a70b072fc0ee479a9488cef3380361c79
EAOErmak <zrmek.aidarbekov@gmail.com>
2026-02-11 20:19:17 +0500

    Again deleted trash

--- src/main/java/com/example/activity_diary/repository/template/TemplateGoalMetricRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.activity_diary.repository.template;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TemplateGoalMetricRepository extends JpaRepository<TemplateGoalMetric, TemplateGoalMetricId> {
-    void deleteByTemplateId(Long templateId);
-}
--- src/main/java/com/example/activity_diary/repository/template/TemplateGoalTagRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.activity_diary.repository.template;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TemplateGoalTagRepository extends JpaRepository<TemplateGoalTag, TemplateGoalTagId> {
-    void deleteByTemplateId(Long templateId);
-}
